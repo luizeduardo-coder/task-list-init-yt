dfd452222c2d72baf98a76ce2fbe5b0c37a22276
Luiz Eduardo <luiz.eduardo@corp.invenzi.com>
2026-08-28 14:05:54 -0300

    Ajusta vite.config e adiciona vercel.json para rotas

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "taskList", // Substitua pelo nome exato do repositório no GitHub
+  base: "/",
 });
